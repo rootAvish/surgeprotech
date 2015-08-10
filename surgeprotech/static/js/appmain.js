7e53d0d582819574184010f14c058f8c44972047
Avishkar Gupta <rootavish@gmail.com>
2015-08-10 16:52:37 +0530

    version 1.0

--- surgeprotech/static/js/appmain.js
+++ surgeprotech/static/js/appmain.js
@@ -21,33 +21,43 @@ angular.module('tabControls', [])
 
 angular.module('CommitteeList',[])
 
+.controller('memberController', ['$scope', function($scope){
+
+	$scope.technical = true;
+
+	$scope.change = function() {
+		$scope.technical = !$scope.technical;
+	};
+	
+}])
+
 .controller('tcMemberController',['$scope', function($scope){
 	
 	$scope.tcmembers = {
-		"CEA": ['Mr. S.K.Mahapatra'],
-		"CPRI":	['Ms. Kanyakumari'],
-		"GETCO": ['Mr. Y.V.Joshi'],
-		"KPTCL": ['Mr. S. Sumanth'],
-		"NTPC": ['Mr. Hirdesh Gupta'],
-		"POWERGRID": ['Mr. A.P.Gangadharan'],
-		'Raychem': ['Mr. V.Sasikumar'],
-		'REC':['Mr. P.J.Thakkar','Mr. Sanjay Kulshrestha']
+		"CEA": 'Mr. S.K.Mahapatra',
+		"CPRI":	'Ms. Kanyakumari',
+		"GETCO": 'Mr. Y.V.Joshi',
+		"KPTCL": 'Mr. S. Sumanth',
+		"NTPC": 'Mr. Hirdesh Gupta',
+		"POWERGRID": 'Mr. A.P.Gangadharan',
+		'Raychem': 'Mr. V.Sasikumar',
+		'REC':'Mr. P.J.Thakkar'
 	};
 
 }])
 
 .controller('ocMemberController',['$scope', function($scope){
+
 	$scope.ocmembers = {
-	'APTRANSCO' : ['Mr. Janardhan Reddy'],
-	'Chairman,BIS- ET 30': ['Mr. N.S.Sodha'],
-	'Secretariat,BIS ET 30': ['Mr. S.S.Yadav'],
-	'Crompton Greaves' : ['Mr. Milind Zodage'],
-	'Elektrolites' : ['Mr. Anil Saboo'],
-	'ERDA': [''],
-	'Lamco': ['Mr. Padma Kumar'],
-	'Lamer Power': ['Mr. Mukesh Mallik'],
-	'MSEDCL': [''],
-	'POWERGRID': ['Mr. Ravi Roshan Kumar']
+	'APTRANSCO' : 'Mr. Janardhan Reddy',
+	'Chairman,BIS- ET 30': 'Mr. N.S.Sodha',
+	'Secretariat,BIS ET 30': 'Mr. S.S.Yadav',
+	'Crompton Greaves' :'Mr. Milind Zodage',
+	'Elektrolites' : 'Mr. Anil Saboo',
+	'ERDA': 'Mr. Tritha Vishwakarma',
+	'Lamco': 'Mr. Padma Kumar',
+	'Lamer Power': 'Mr. Mukesh Mallik',
+	'MSEDCL': 'Mr. Pratik Bhandarkar'
 	};
 }]);
 
