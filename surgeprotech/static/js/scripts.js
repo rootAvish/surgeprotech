a541a29165842ae0e34299c390daa44d14aa2b9f
Avishkar Gupta <rootavish@gmail.com>
2015-08-26 07:26:35 +0530

    More changes to the frontend main page

--- surgeprotech/static/js/scripts.js
+++ surgeprotech/static/js/scripts.js
@@ -1,17 +1,43 @@
 $('#header').onePageNav({
     currentClass: 'current',
-    changeHash: false,
-    scrollSpeed: 750,
+    changeHash: true,
+    scrollSpeed: 200,
     scrollThreshold: 0.5,
     filter: '',
-    easing: 'swing',
-    begin: function() {
-        //I get fired when the animation is starting
-    },
-    end: function() {
-        //I get fired when the animation is ending
-    },
-    scrollChange: function($currentListItem) {
-        //I get fired when you enter a section and I pass the list item of the section
+    easing: 'linear'
+});
+
+$('.animated').appear(function(){
+     var element = $(this);
+     var animation = element.data('animation');
+     var animationDelay = element.data('delay');
+     if (animationDelay) {
+       setTimeout(function(){
+         element.addClass( animation + " visible" );
+         element.removeClass('hiding');
+         if (element.hasClass('counter')) {
+           element.find('.value').countTo();
+         }
+       }, animationDelay);
+     }else {
+       element.addClass( animation + " visible" );
+       element.removeClass('hiding');
+       if (element.hasClass('counter')) {
+         element.find('.value').countTo();
+       }
+     }    
+   },{accY: -150});
+
+var listfull = false;
+
+$('#member-list-toggle').click(function(event) {
+
+    if (listfull == false) {
+        $('#member-list-toggle').html('SEE LESS<i class="fa fa-arrow-up"></i>');
+        listfull = true;
+    }
+    else {
+        $('#member-list-toggle').html('SEE ALL<i class="fa fa-arrow-down"></i>');
+        listfull = false;
     }
 });
